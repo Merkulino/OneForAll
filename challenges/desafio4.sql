769996d0a265f4ec9f13a5c6b830fd26dba2a56f
Melqui Brito <Merkulino11@gmail.com>
2023-03-05 14:09:28 -0300

    Requisito 4 revisto e check

--- challenges/desafio4.sql
+++ challenges/desafio4.sql
@@ -1,7 +1,9 @@
--- Incompleto
-
-SELECT username AS `pessoa_usuaria`,
-	IF(YEAR(his.play_date) >= 2021, 'Ativa', 'Inativa')
+SELECT 
+	username AS `pessoa_usuaria`,
+	-- YEAR(his.play_date) AS `ano`
+	IF(YEAR(MAX(his.play_date)) >= '2021', 'Ativa', 'Inativa') AS `status_pessoa_usuaria`
 FROM user AS u
 INNER JOIN history AS his
-ON his.user_id = u.id;
+ON his.user_id = u.id
+GROUP BY pessoa_usuaria
+ORDER BY pessoa_usuaria;
